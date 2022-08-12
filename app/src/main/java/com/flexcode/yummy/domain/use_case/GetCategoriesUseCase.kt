26fd77d668f55b08dae9f479dc7895e46b9e4cf6
jxareas <areasjx@gmail.com>
2022-08-12 15:19:00 -0600

    Remove AppModule

--- app/src/main/java/com/flexcode/yummy/domain/use_case/GetCategoriesUseCase.kt
+++ app/src/main/java/com/flexcode/yummy/domain/use_case/GetCategoriesUseCase.kt
@@ -3,9 +3,11 @@ package com.flexcode.yummy.domain.use_case
 import com.flexcode.yummy.domain.models.Categories
 import com.flexcode.yummy.domain.repository.CategoriesRepository
 import com.flexcode.yummy.utils.Resource
+import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+@ViewModelScoped
 class GetCategoriesUseCase @Inject constructor(
     private val repository: CategoriesRepository,
 ) {
