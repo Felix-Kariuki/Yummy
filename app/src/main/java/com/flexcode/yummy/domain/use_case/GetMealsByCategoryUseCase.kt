afa99158ae1533960b473c7a444d7a9d727a515a
Tonnie <vontonnie@gmail.com>
2022-10-26 18:02:21 +0300

    Annotate GetMealsByCategoryUseCase with @ViewModelScoped annotation

--- app/src/main/java/com/flexcode/yummy/domain/use_case/GetMealsByCategoryUseCase.kt
+++ app/src/main/java/com/flexcode/yummy/domain/use_case/GetMealsByCategoryUseCase.kt
@@ -1,13 +1,15 @@
 package com.flexcode.yummy.domain.use_case
 
-import com.flexcode.yummy.domain.models.Categories
 import com.flexcode.yummy.domain.models.Meals
 import com.flexcode.yummy.domain.repository.MealsRepository
+import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class GetMealsByCategoryUseCase (private val repository: MealsRepository) {
+@ViewModelScoped
+class GetMealsByCategoryUseCase @Inject constructor(private val repository: MealsRepository) {
 
-    operator fun invoke(category:String):Flow<List<Meals>>{
+    operator fun invoke(category: String): Flow<List<Meals>> {
         return repository.getMealsByCategory(category = category)
     }
 
