2c798c1e074472eb7070969e42c8498997587e81
Tonnie <vontonnie@gmail.com>
2022-10-26 19:25:16 +0300

    Add OnClickClearText object t MealsEvent sealed class

--- app/src/main/java/com/flexcode/yummy/presentation/meals_screen/MealsEvent.kt
+++ app/src/main/java/com/flexcode/yummy/presentation/meals_screen/MealsEvent.kt
@@ -3,5 +3,6 @@ package com.flexcode.yummy.presentation.meals_screen
 sealed class MealsEvent {
     data class OnSearchMeal(val meal: String) : MealsEvent()
     object Refresh : MealsEvent()
+    object OnClickClearText : MealsEvent()
     data class OnClickCategoryItem(val category: String) : MealsEvent()
 }
