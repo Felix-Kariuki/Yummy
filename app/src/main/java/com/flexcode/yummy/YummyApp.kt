8820b725aff6624421eb3fe16a4c234a809b5729
Felix-Kariuki <felixmurimi280@gmail.com>
2022-11-28 18:36:15 +0300

    Crashlytics

--- app/src/main/java/com/flexcode/yummy/YummyApp.kt
+++ app/src/main/java/com/flexcode/yummy/YummyApp.kt
@@ -1,14 +1,28 @@
 package com.flexcode.yummy
 
 import android.app.Application
+import com.flexcode.yummy.crashlytics.CrashlyticsTree
 import dagger.hilt.android.HiltAndroidApp
+import org.jetbrains.annotations.NotNull
 import timber.log.Timber
 
 @HiltAndroidApp
 class YummyApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG)
-            Timber.plant(Timber.DebugTree())
+        initTimber()
+    }
+
+    private fun initTimber() = when {
+        BuildConfig.DEBUG -> {
+            Timber.plant(object : Timber.DebugTree() {
+                override fun createStackElementTag(@NotNull element: StackTraceElement): String {
+                    return super.createStackElementTag(element) + ":" + element.lineNumber
+                }
+            })
+        }
+        else -> {
+            Timber.plant(CrashlyticsTree())
+        }
     }
 }
